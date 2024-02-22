e3b608b1f1b6b61648545407378af3f9938ecf52
Nicholas Odhiambo <cicada@localhost.localdomain>
2024-02-22 14:23:03 +0300

    week one

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# Create a python file to accept user input
+
+# Ask for name 
+name = input('What is your name? ') 
+
+# Ask for age
+age = input('What is your age? ')
+
+# Ask for location
+location = input('Where are you located? ') 
+
+# Print out the result 
+print(f'Hello {name}, you are {age} years old and live in {location}.')
